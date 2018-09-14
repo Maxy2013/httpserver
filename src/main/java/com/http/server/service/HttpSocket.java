17a8c663617a6627a72af7c1b6e4cea8b326629e
刘正 <liu.zh_neu@neusoft.com>
2018-09-14 17:57:38 +0800

    httpserver 文档及作业

--- src/main/java/com/http/server/service/HttpSocket.java
+++ src/main/java/com/http/server/service/HttpSocket.java
@@ -11,28 +11,29 @@ import java.net.Socket;
  * @author lz
  * 2018/9/14 15:22
  */
-public class HttpSocket {
+public class HttpSocket{
 
-    private static final Integer PORT = 80;
+    private static final Integer PORT = 8021;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HttpSocket.class);
 
     public static void main(String[] args) {
-        startServer();
+        while (PORT > 1){
+            startServer();
+        }
     }
 
-    public static void startServer(){
+    private static void startServer(){
         ServerSocket serverSocket = null;
         try {
             //创建serverSocket实例
-            serverSocket = new ServerSocket(PORT);
-            while (PORT > 1){
-                readSocketInfo(serverSocket);
-            }
+            serverSocket = new ServerSocket(8021);
+
+            readSocketInfo(serverSocket);
             serverSocket.close();
         } catch (Exception e) {
             e.printStackTrace();
-        }/*finally {
+        }finally {
             try {
                 if (null != serverSocket){
                     serverSocket.close();
@@ -40,7 +41,7 @@ public class HttpSocket {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }*/
+        }
     }
 
     private static void readSocketInfo(ServerSocket serverSocket) throws Exception{
@@ -51,22 +52,26 @@ public class HttpSocket {
 
         String lineInput;
         String requestPage = null;
+        String userInf = null;
         while ((lineInput = in.readLine()) != null){
             LOGGER.debug("line information: {}", lineInput);
             if(in.getLineNumber() == 1){
                 requestPage = lineInput.substring(lineInput.indexOf('/') + 1, lineInput.lastIndexOf(' '));
                 LOGGER.debug("requestPage:{}", requestPage);
             }else {
-                if (lineInput.isEmpty()){
+                if(lineInput.startsWith("Cookie: ")){
+                    userInf = lineInput;
+                    LOGGER.debug("new user --->>{}", userInf);
+                }else if (lineInput.isEmpty()){
                     LOGGER.info("header finished!");
-                    doResponseGet(requestPage, socket);
+                    doResponseGet(userInf, requestPage, socket);
                 }
             }
         }
 
     }
 
-    private static void doResponseGet(String requestPage, Socket socket) throws Exception {
+    private static void doResponseGet(String userInf, String requestPage, Socket socket) throws Exception {
         final String WEB_ROOT = "F:";
         File file = new File(WEB_ROOT, requestPage);
 
@@ -82,15 +87,26 @@ public class HttpSocket {
             socket.close();
             LOGGER.info("request complete!----->>");
         }else {
-            String msg = " 555555.....\r\n";
+            String msg = " 555555.....\r\n" + ((userInf == null) ? "new user..." : userInf);
             String response = "HTTP/1.1 200 OK\r\n";
             response += "Server: Leader Server/0.1\r\n";
+
+            if(null == userInf){
+                response += genCookieHeader();
+            }
+
+
             response += "Content Length: " + (msg.length() - 4) + "\r\n";
             response += "\r\n";
             response += msg;
             out.write(response.getBytes());
             out.flush();
         }
-            out.close();
+//            out.close();
+    }
+
+    private static String genCookieHeader() {
+        String header = "Set-Cookie: ";
+        return header;
     }
 }
